ba5f1dd814b7db6809b69ff96a9721941016056a
rcc <robert@textensor.com>
2013-02-08 09:06:18 +0000

    Bounce example with default execution method

---
+++ src/org/lemsml/examplemains/BounceConditional.java
@@ -0,0 +1,20 @@
+package org.lemsml.examplemains;
+
+ 
+
+public final class BounceConditional {
+	
+	
+	private BounceConditional() {
+		
+	}
+	
+	public static void main(String[] argv) {
+	
+		VizExample.run("bounce-conditional.xml");
+	
+	}
+	 
+    
+    
+}
