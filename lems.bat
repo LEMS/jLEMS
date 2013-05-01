6874698b4f8ace4d5b933757000cd8fdb94812bd
Padraig Gleeson <p.gleeson@gmail.com>
2013-05-01 14:31:40 +0100

    Correctly updated version nums this time

--- lems.bat
+++ lems.bat
@@ -1,4 +1,6 @@
-set CLASSPATH=.;.\target\jlems-0.9.4.jar;%LEMS_HOME%\target\jlems-0.9.4.jar
+set LEMS_VERSION=0.9.5
+
+set CLASSPATH=.;.\target\jlems%LEMS_VERSION%-.jar;%LEMS_HOME%\target\jlems-%LEMS_VERSION%.jar
 
 echo Running the LEMS application...
 
