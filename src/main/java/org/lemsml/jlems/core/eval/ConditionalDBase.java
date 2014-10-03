df47241ea7a395bec6ee2bcdffff79fb751cb7d0
Padraig Gleeson <p.gleeson@gmail.com>
2014-10-03 17:16:43 +0100

    More informative string on ConditionalDBase.getExpressionString()

--- src/main/java/org/lemsml/jlems/core/eval/ConditionalDBase.java
+++ src/main/java/org/lemsml/jlems/core/eval/ConditionalDBase.java
@@ -11,110 +11,108 @@ import org.lemsml.jlems.core.run.RuntimeError;
 
 public class ConditionalDBase implements DoubleEvaluator {
 
-	 
-	ArrayList<BooleanEvaluator> conditions = new ArrayList<BooleanEvaluator>();
-	ArrayList<DoubleEvaluator> values = new ArrayList<DoubleEvaluator>();
-	
-	 
+    ArrayList<BooleanEvaluator> conditions = new ArrayList<BooleanEvaluator>();
+    ArrayList<DoubleEvaluator> values = new ArrayList<DoubleEvaluator>();
 
-   
-	public void addCondition(BooleanEvaluator be, DoubleEvaluator de) {
-		//E.info("Added condidtion " + be + " " + de);
-		conditions.add(be);
-		values.add(de);
-	}
-	
-	
-	
+    public void addCondition(BooleanEvaluator be, DoubleEvaluator de) {
+        //E.info("Added condidtion " + be + " " + de);
+        conditions.add(be);
+        values.add(de);
+    }
+
     @Override
     public String toString() {
         return "ConditionalDBase{}";
     }
 
-    
-  
-	public double evalD(HashMap<String, Double> valHM) {
-		double ret = 0.;
-		for (int i = 0; i < conditions.size(); i++) {
-			if (conditions.get(i).evalB(valHM)) {
-				ret = values.get(i).evalD(valHM);
-				break;
-			}
-		}
-		return ret;
-	}
-	
-	
-	public double evalptr(HashMap<String, DoublePointer> valptrHM) throws RuntimeError {
-		double ret = 0.;
-		for (int i = 0; i < conditions.size(); i++) {
-			if (conditions.get(i).evalptr(valptrHM)) {
-				ret = values.get(i).evalptr(valptrHM);
-				break;
-			}
-		}
-		return ret;
-	}
-	
+    @Override
+    public double evalD(HashMap<String, Double> valHM) {
+        double ret = 0.;
+        for (int i = 0; i < conditions.size(); i++) {
+            if (conditions.get(i).evalB(valHM)) {
+                ret = values.get(i).evalD(valHM);
+                break;
+            }
+        }
+        return ret;
+    }
 
-	public double evalptr(HashMap<String, DoublePointer> valptrHM, HashMap<String, DoublePointer> v2HM) {
-		double ret = 0.;
-		E.missing();
-		/*
-		for (int i = 0; i < conditions.size(); i++) {
-			if (conditions.get(i).evalptr(valptrHM, v2HM)) {
-				ret = values.get(i).evalptr(valptrHM, v2HM);
-				break;
-			}
-		}
-		*/
-		return ret;
-	}
+    @Override
+    public double evalptr(HashMap<String, DoublePointer> valptrHM) throws RuntimeError {
+        double ret = 0.;
+        for (int i = 0; i < conditions.size(); i++) {
+            if (conditions.get(i).evalptr(valptrHM)) {
+                ret = values.get(i).evalptr(valptrHM);
+                break;
+            }
+        }
+        return ret;
+    }
 
-	public ConditionalDBase makeCopy() {
-		ConditionalDBase ret = new ConditionalDBase();
-		for (int i = 0; i < conditions.size(); i++) {
-			ret.addCondition(conditions.get(i), values.get(i));
-		}
-		return ret;
-	}
+    @Override
+    public double evalptr(HashMap<String, DoublePointer> valptrHM, HashMap<String, DoublePointer> v2HM) {
+        double ret = 0.;
+        E.missing();
+        /*
+         for (int i = 0; i < conditions.size(); i++) {
+         if (conditions.get(i).evalptr(valptrHM, v2HM)) {
+         ret = values.get(i).evalptr(valptrHM, v2HM);
+         break;
+         }
+         }
+         */
+        return ret;
+    }
 
-	public ConditionalDBase makePrefixedCopy(String pfx, HashSet<String> stetHS) {
-		E.missing();
-		return null;
-	}
+    public ConditionalDBase makeCopy() {
+        ConditionalDBase ret = new ConditionalDBase();
+        for (int i = 0; i < conditions.size(); i++) {
+            ret.addCondition(conditions.get(i), values.get(i));
+        }
+        return ret;
+    }
 
-	public void substituteVariableWith(String vnm, String pth) {
-		E.missing();
-	}
+    @Override
+    public ConditionalDBase makePrefixedCopy(String pfx, HashSet<String> stetHS) {
+        E.missing();
+        return null;
+    }
 
-	public boolean variablesIn(HashSet<String> known) {
-		E.missing();
-		return false;
-	}
+    @Override
+    public void substituteVariableWith(String vnm, String pth) {
+        E.missing();
+    }
 
+    @Override
+    public boolean variablesIn(HashSet<String> known) {
+        E.missing();
+        return false;
+    }
 
-	@Override
-	public String getExpressionString() {
-        // is this needed??
-		//E.missing();
-		return "ConditionalDBase with conditions: "+conditions+" and corresponding values: "+values;
-	}
+    @Override
+    public String getExpressionString() {
+        //E.missing();
+        String info = "ConditionalDBase:";
+        for (int i = 0; i < conditions.size(); i++) {
+            info = info + " (IF "+conditions.get(i).getExpressionString() +" THEN " + values.get(i).getExpressionString()+ ")";
+        }
+        return info;
+    }
 
-	@Override
-	public String getReversePolishExpressionString() {
-		E.missing();
-		return null;
-	}
-	
-	public boolean isTrivial() {
-		return false;
-	}
+    @Override
+    public String getReversePolishExpressionString() {
+        E.missing();
+        return null;
+    }
 
+    @Override
+    public boolean isTrivial() {
+        return false;
+    }
 
-	@Override
-	public String getSimpleValueName() {
-		E.error("Shouldnt look for simple value on a condition");
-		return null;
-	}
+    @Override
+    public String getSimpleValueName() {
+        E.error("Shouldnt look for simple value on a condition");
+        return null;
+    }
 }
